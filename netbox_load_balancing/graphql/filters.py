8bc6a1c8c4172e6ae511508ddc402c5ca701140a
andy <andy@shady.org>
2025-05-05 10:01:34 +0300

    Implemented GraphQL enums and filters compatible with NetBox 4.3

--- netbox_load_balancing/graphql/filters.py
+++ netbox_load_balancing/graphql/filters.py
@@ -94,9 +94,12 @@ class NetBoxLoadBalancingHealthMonitorFilter(
         | None
     ) = strawberry_django.filter_field()
     http_secure: FilterLookup[bool] | None = strawberry_django.filter_field()
-    http_response_codes: IntegerArrayLookup[int] | None = (
-        strawberry_django.filter_field()
-    )
+    http_response_codes: (
+        Annotated[
+            "IntegerArrayLookup", strawberry.lazy("netbox.graphql.filter_lookups")
+        ]
+        | None
+    ) = strawberry_django.filter_field()
     probe_interval: FilterLookup[int] | None = strawberry_django.filter_field()
     response_timeout: FilterLookup[int] | None = strawberry_django.filter_field()
     disabled: FilterLookup[bool] | None = strawberry_django.filter_field()
@@ -167,11 +170,11 @@ class NetBoxLoadBalancingVirtualIPFilter(ContactFilterMixin, NetBoxModelFilterMi
     dns_name: FilterLookup[str] | None = strawberry_django.filter_field()
     disabled: FilterLookup[bool] | None = strawberry_django.filter_field()
     virtual_pool: (
-            Annotated[
-                "NetBoxLoadBalancingVirtualIPPoolFilter",
-                strawberry.lazy("netbox_load_balancing.graphql.filters"),
-            ]
-            | None
+        Annotated[
+            "NetBoxLoadBalancingVirtualIPPoolFilter",
+            strawberry.lazy("netbox_load_balancing.graphql.filters"),
+        ]
+        | None
     ) = strawberry_django.filter_field()
     address: (
         Annotated["IPAddressFilter", strawberry.lazy("ipam.graphql.filters")] | None
